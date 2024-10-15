7a62be261a6f656e11ac24eb24fb166a06421484
Dmitriy <125189134+1abobik1@users.noreply.github.com>
2024-10-15 17:56:31 +0300

    Удалить фотографии сотрудников клуба, вместо них аватары с инициалами

--- client/src/components/TeamPageComponents/BigBosses.tsx
+++ client/src/components/TeamPageComponents/BigBosses.tsx
@@ -1,60 +1,40 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import data from "./boss.json"
-import { Card, CardContent, Typography, Button } from '@mui/material';
+import {Avatar, Card, CardContent, Typography} from '@mui/material';
 import './BigBosses.css'
-import boss1 from "../../images/boss1.jpg"
-import boss2 from "../../images/boss2.jpg"
-import boss3 from "../../images/boss3.jpg"
-import boss4 from "../../images/boss4.jpg"
-import boss5 from "../../images/boss5.jpg"
-import boss6 from "../../images/boss6.jpg"
-import boss7 from "../../images/boss7.png"
 
-interface BossType{
-    id:number;
-    name:string;
-    role:string;
-    image:string;
+interface BossType {
+    id: number;
+    name: string;
+    role: string;
 }
 
-
-const imageMapping: Record<string, string> = {
-    boss1: boss1,
-    boss2: boss2,
-    boss3:boss3,
-    boss4:boss4,
-    boss5:boss5,
-    boss6:boss6,
-    boss7:boss7,
-};
+// Photos of the club staff are not stored in the repository: an avatar with initials is shown instead.
+function initials(fullName: string): string {
+    const [lastName = '', firstName = ''] = fullName.split(' ');
+    return `${firstName.charAt(0)}${lastName.charAt(0)}`.toUpperCase();
+}
 
 const BigBosses = () => {
-    const [boss, setBoss] = useState<BossType[]>([]);
-
-    useEffect(() => {
-        setBoss(data.boss);
-    }, []);
-
+    const bosses: BossType[] = data.boss;
 
     return (
         <div className="boss-container">
 
-            <h1 style={{color:"white",fontSize:"30px"}}>Представители клуба</h1>
+            <h1 style={{color: "white", fontSize: "30px"}}>Представители клуба</h1>
 
             <div className="boss-content">
-          {boss.map((item) => (
-            <Card key={item.id} className="boss-card">
-              <img src={imageMapping[item.image]} alt={item.name} className="boss-image" />
-              <CardContent>
-                <Typography variant="h6">{item.name}</Typography>
-                <Typography variant="body2">{item.role}</Typography>
-
-              </CardContent>
-            </Card>
-          ))}
+                {bosses.map((item) => (
+                    <Card key={item.id} className="boss-card">
+                        <Avatar className="boss-avatar" alt={item.name}>{initials(item.name)}</Avatar>
+                        <CardContent>
+                            <Typography variant="h6">{item.name}</Typography>
+                            <Typography variant="body2">{item.role}</Typography>
+                        </CardContent>
+                    </Card>
+                ))}
+            </div>
         </div>
-  </div>
-
     );
 };
 
